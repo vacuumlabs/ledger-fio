afebed89924f4a3c7afc66bc31e9c9345191dcec
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2021-11-19 13:16:42 +0100

    Remove ledgerjs

--- ledgerjs-fio/src/utils/assert.ts
+++
@@ -1,5 +0,0 @@
-import {ErrorBase} from "../errors"
-
-export function assert(cond: boolean, errMsg: string): asserts cond {
-    if (!cond) throw new ErrorBase('Assertion failed' + errMsg ? ': ' + errMsg : '')
-}
