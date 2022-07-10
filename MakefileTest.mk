37a36cc9df503a766925581dc0a3da77ec322489
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2022-07-10 11:05:29 +0200

    Remaddress

--- MakefileTest.mk
+++ MakefileTest.mk
@@ -39,6 +39,7 @@ speculos_port_5001_test_internal:
 	$(call run_nodejs_test,5001,40001,signTransactionNewfundsreq.js)
 	$(call run_nodejs_test,5001,40001,signTransactionRecordobt.js)
 	$(call run_nodejs_test,5001,40001,signTransactionAddaddress.js)
+	$(call run_nodejs_test,5001,40001,signTransactionRemaddress.js)
 	@echo "# ALL TESTS COMPLETED!" | tee -a speculos-port-5001.log
 
 .PHONY: speculos_port_5001_unit_test_internal
