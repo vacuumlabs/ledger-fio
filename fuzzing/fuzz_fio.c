46b2edf75009697267a6403b8f3846510608d3eb
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2023-01-23 13:06:22 +0100

    Fuzzer modified with very basic new corpus

--- fuzzing/fuzz_fio.c
+++ fuzzing/fuzz_fio.c
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#include <stdio.h>
+
 static const int INS_NONE = -1;
 
 typedef struct {
@@ -10,39 +12,38 @@ typedef struct {
     uint8_t ins;
     uint8_t p1;
     uint8_t p2;
-    uint8_t lc;
+    uint8_t lc; //will be ignored    
 } header_t;
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     // The size of the data should exceed the length of the header
     if (Size >= sizeof(header_t)){
         header_t * header = (header_t *) Data;
-
         uint8_t* data = (uint8_t *)(Data + sizeof(*header));
-
+        size_t data_size = Size - sizeof(*header);
+        
+        printf("%hhx, %hhx, %hhx, %hhx\n", header->cla, header->ins, header->p1, header->p2);
+        
         BEGIN_TRY {
             TRY {
                 handler_fn_t* handlerFn = lookupHandler(header->ins);
 
                 VALIDATE(handlerFn != NULL, ERR_UNKNOWN_INS);
+                printf("Hander found\n");
 
-                bool isNewCall = false;
-                if (currentInstruction == INS_NONE) {
-                    explicit_bzero(&instructionState, SIZEOF(instructionState));
-                    isNewCall = true;
-                    currentInstruction = header->ins;
-                } else {
-                    VALIDATE(header->ins == currentInstruction, ERR_STILL_IN_CALL);
-                }
+                explicit_bzero(&instructionState, SIZEOF(instructionState));
+                currentInstruction = header->ins;
 
-                handlerFn(header->p1, header->p2, data, header->lc, isNewCall);
+                printf("Running handler\n");
+                handlerFn(header->p1, header->p2, data, data_size, true);
+                printf("Handler ran\n");
             }
             CATCH_OTHER(e) {
             }
             FINALLY {
             }
-        }
         END_TRY;
+        }
     }
     
     return 0;
